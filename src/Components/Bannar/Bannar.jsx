35be8085ae10fc0e0f80ed9e6c772730f9a0506a
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2024-05-13 21:07:49 +0600

    one extra section done

--- src/Components/Bannar/Bannar.jsx
+++ src/Components/Bannar/Bannar.jsx
@@ -1,13 +1,17 @@
 import { Fade } from "react-awesome-reveal";
+import { Link } from "react-router-dom";
 
 
 const Bannar = () => {
+    const scrollToTop = () => {
+        window.scrollTo(0, 0)
+    }
     return (
         <div className=" container mx-auto mt-16">
             <div className="bg-[url('https://i.ibb.co/HgG56bZ/question-mark-in-front-of-banner-background-vector.jpg')] h-[500px] bg-no-repeat object-cover bg-center w-full">
                 <div className="flex flex-col gap-8 justify-center items-center  px-10 h-[500px] bg-gradient-to-r from-[#151515] to-[rgba(21,21,21,0)]">
                     <h1 className="lg:text-5xl text-2xl md:text-3xl text-white font-medium">Find your queries now!</h1>
-                    <button className="lg:text-lg md:text-lg btn hover:text-primary-color hover:bg-white hover:border-primary-color bg-primary-color text-white font-medium  border-none px-5"> All Queries</button>
+                    <Link to={'/queries'}><button onClick={scrollToTop} className="lg:text-lg md:text-lg btn hover:text-primary-color hover:bg-white hover:border-primary-color bg-primary-color text-white font-medium  border-none px-5"> All Queries</button></Link>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,13 +4,14 @@ import { Helmet } from 'react-helmet-async';
 import Bannar from '../../Components/Bannar/Bannar';
 import axios from 'axios';
 import RecentQueryCard from '../../Components/RecentQueryCard/RecentQueryCard';
+import Marquee from "react-fast-marquee";
 
 const Home = () => {
     const [recentQueries, setRecentQueries] = useState([]);
 
     useEffect(() => {
         axios.get(`${import.meta.env.VITE_API_URL}/recent-six`)
-        .then(data => setRecentQueries(data.data))
+            .then(data => setRecentQueries(data.data))
     }, [])
     // console.log(recentQueries)
     return (
@@ -25,7 +26,7 @@ const Home = () => {
             </div>
 
             <div className='container mx-auto space-y-8 mt-16'>
-                <h1 className='text-4xl text-center'>Recent Queries</h1>
+                <h1 className='text-4xl font-medium text-center'>Recent Queries</h1>
                 <div className='flex justify-center'>
                     <p className='text-center lg:w-2/3 md:w-2/3'>Explore the latest queries across diverse product lines, gaining insights into varying customer interests and preferences. Uncover valuable trends and demands unique to each product category, guiding strategic decisions and optimizations</p>
                 </div>
@@ -36,6 +37,32 @@ const Home = () => {
                     }
                 </div>
 
+            </div>
+
+            <div className='mt-20 mb-12 space-y-10'>
+                <h1 className='text-4xl text-center font-medium'>Some Trusted Brands</h1>
+                <Marquee >
+                    <div className='flex gap-10'>
+                        <div className=' w-[200px] h-[110px] bg-white'>
+                            <img className='w-full h-full object-cover' src={'https://i.ibb.co/pdWhVjv/1562575696.jpg'} alt="" />
+                        </div>
+                        <div className='w-[200px] h-[110px]  bg-white'>
+                            <img className='w-full h-full object-cover' src={'https://i.ibb.co/8NnJTb8/share-default.jpg'} alt="" />
+                        </div>
+                        <div className='w-[200px] h-[110px]  bg-white'>
+                            <img className='w-full h-full object-cover' src={'https://i.ibb.co/2qYZFFK/877951b20ab64005a25e03c6f7cfeaca.jpg'} alt="" />
+                        </div>
+                        <div className='w-[200px] h-[110px]  bg-white'>
+                            <img className='w-full h-full object-cover' src={'https://i.ibb.co/ccqWKy6/brand.gif'} alt="" />
+                        </div>
+                        <div className='w-[200px] h-[110px] bg-white'>
+                            <img className='w-full h-full object-cover' src={'https://i.ibb.co/RbKk9zC/db1246b017ecc82923191111ede44df2.png'} alt="" />
+                        </div>
+                        <div className='w-[200px] h-[110px] bg-white'>
+                            <img className='w-full h-full object-cover' src={'https://i.ibb.co/Tw0HT8p/image.png'} alt="" />
+                        </div>
+                    </div>
+                </Marquee>
             </div>
         </div>
 
