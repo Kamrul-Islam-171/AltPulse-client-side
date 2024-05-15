e4527a6e96947fd087d93d2539d2fbf1a9eda792
Kamrul-Islam-171 <kamrul.ruet.171@gmail.com>
2024-05-15 17:09:46 +0600

    react awesome reveal used

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,7 +5,8 @@ import Bannar from '../../Components/Bannar/Bannar';
 import axios from 'axios';
 import RecentQueryCard from '../../Components/RecentQueryCard/RecentQueryCard';
 import Marquee from "react-fast-marquee";
-import { motion } from "framer-motion"
+
+import { Fade } from "react-awesome-reveal";
 
 import NavBar2 from '../../Components/Navbar2/NavBar2';
 
@@ -47,70 +48,84 @@ const Home = () => {
 
                 </div>
                 <div className='text-center flex flex-col justify-center items-center space-y-5'>
-                    <h1 className='lg:text-6xl text-4xl font-medium'>Search for an idea</h1>
-                    <p className='text-2xl'>What do you want to try next? Think of something you’re into—like and see what you find.</p>
-                    <div className='border rounded-full border-primary-color'>
-                        <button className="lg:text-lg md:text-lg btn hover:text-primary-color hover:bg-white hover:border-primary-color bg-primary-color text-white font-medium rounded-full border-none px-5"> Explore</button>
-                    </div>
+                    <Fade direction='down' duration={1000}><h1 className='lg:text-6xl text-4xl font-medium'>Search for an idea</h1></Fade>
+                    <Fade direction='up' duration={1000}><p className='text-2xl'>What do you want to try next? Think of something you’re into—like and see what you find.</p>
+                        <div className='border rounded-full border-primary-color'>
+                            <button className="lg:text-lg md:text-lg btn hover:text-primary-color hover:bg-white hover:border-primary-color bg-primary-color text-white font-medium rounded-full border-none px-5"> Explore</button>
+                        </div>
+                    </Fade>
+
 
                 </div>
             </div>
 
             <div className='container mx-auto mt-28 grid lg:grid-cols-3 md:grid-cols-2 gap-8'>
-                <div className='space-y-3   px-8 shadow-xl py-12 rounded-md'>
-                    <h3 className='text-2xl font-medium'>Popular Categories</h3>
-                    <div className='h-[2px] bg-primary-color'></div>
-                    <div>
-                        <ul className='ml-3 space-y-1 list-disc'>
-                            <li>Technology and Electronics</li>
-                            <li>Fashion and Apparel</li>
-                            <li>Beauty and Personal Care</li>
-                            <li>Health and Wellness</li>
-                            <li>Food and Beverages</li>
-                            <li>Sports and Outdoors</li>
-                        </ul>
+                <Fade duration={2000} cascade damping={0.1} >
+                    <div className='space-y-3   px-8 shadow-xl py-12 rounded-md'>
+                        <h3 className='text-2xl font-medium'>Popular Categories</h3>
+                        <div className='h-[2px] bg-primary-color'></div>
+                        <div>
+                            <ul className='ml-3 space-y-1 list-disc'>
+                                <li>Technology and Electronics</li>
+                                <li>Fashion and Apparel</li>
+                                <li>Beauty and Personal Care</li>
+                                <li>Health and Wellness</li>
+                                <li>Food and Beverages</li>
+                                <li>Sports and Outdoors</li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div className='space-y-3   px-8 shadow-xl py-12 rounded-md'>
-                    <h3 className='text-2xl font-medium'>Trending Categories</h3>
-                    <div className='h-[2px] bg-primary-color'></div>
-                    <div>
-                        <ul className='ml-3 space-y-1 list-disc'>
-                            <li>Sustainable Living</li>
-                            <li>Smart Home Devices</li>
-                            <li>Plant-Based and Vegan Products</li>
-                            <li>Fitness and Wellness Products</li>
-                            <li>Food and Beverages</li>
-                            <li>Sports and Outdoors</li>
-                        </ul>
+                </Fade>
+                <Fade duration={2000} cascade damping={0.1}>
+                    <div className='space-y-3   px-8 shadow-xl py-12 rounded-md'>
+                        <h3 className='text-2xl font-medium'>Trending Categories</h3>
+                        <div className='h-[2px] bg-primary-color'></div>
+                        <div>
+                            <ul className='ml-3 space-y-1 list-disc'>
+                                <li>Sustainable Living</li>
+                                <li>Smart Home Devices</li>
+                                <li>Plant-Based and Vegan Products</li>
+                                <li>Fitness and Wellness Products</li>
+                                <li>Food and Beverages</li>
+                                <li>Sports and Outdoors</li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-                <div className='space-y-3   px-8 shadow-xl py-12 rounded-md'>
-                    <h3 className='text-2xl font-medium'>Latest Categories</h3>
-                    <div className='h-[2px] bg-primary-color'></div>
-                    <div>
-                        <ul className='ml-3 space-y-1 list-disc'>
-                            <li>Plant-Based Home Cleaning Products</li>
-                            <li>Fashion and Apparel</li>
-                            <li>Beauty and Personal Care</li>
-                            <li>Hydroponic and Indoor Gardening Kits</li>
-                            <li>Natural and Organic Mattresses</li>
-                            <li>Mindful Tech Detox Retreats</li>
-                        </ul>
+                </Fade>
+                <Fade duration={2000} cascade damping={0.1}>
+                    <div className='space-y-3   px-8 shadow-xl py-12 rounded-md'>
+                        <h3 className='text-2xl font-medium'>Latest Categories</h3>
+                        <div className='h-[2px] bg-primary-color'></div>
+                        <div>
+                            <ul className='ml-3 space-y-1 list-disc'>
+                                <li>Plant-Based Home Cleaning Products</li>
+                                <li>Fashion and Apparel</li>
+                                <li>Beauty and Personal Care</li>
+                                <li>Hydroponic and Indoor Gardening Kits</li>
+                                <li>Natural and Organic Mattresses</li>
+                                <li>Mindful Tech Detox Retreats</li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
+                </Fade>
             </div>
 
 
             <div className='lg:pt-8 md:pt-0 '>
-                <Bannar></Bannar>
+                <Fade duration={1000} direction='up'>
+                    <Bannar></Bannar>
+                </Fade>
             </div>
 
+
             <div className='container mx-auto space-y-8 mt-16'>
-                <h1 className='text-4xl font-medium text-center'>Recent Queries</h1>
-                <div className='flex justify-center'>
-                    <p className='text-center lg:w-2/3 md:w-2/3'>Explore the latest queries across diverse product lines, gaining insights into varying customer interests and preferences. Uncover valuable trends and demands unique to each product category, guiding strategic decisions and optimizations</p>
-                </div>
+
+                <Fade duration={1000} direction='up'>
+                    <h1 className='text-4xl font-medium text-center'>Recent Queries</h1>
+                    <div className='flex justify-center'>
+                        <p className='text-center lg:w-2/3 md:w-2/3'>Explore the latest queries across diverse product lines, gaining insights into varying customer interests and preferences. Uncover valuable trends and demands unique to each product category, guiding strategic decisions and optimizations</p>
+                    </div>
+                </Fade>
 
                 <div className='grid lg:grid-cols-3 md:grid-cols-2 gap-5 '>
                     {
@@ -123,57 +138,60 @@ const Home = () => {
 
 
 
+
             <div className='container mx-auto mt-20  font-medium'>
-                <h1 className='text-3xl'>Alternative Product FAQs: Your Questions Answered</h1>
-                <div className='bg-primary-color h-[2px] mt-3 mb-8 '></div>
+                <Fade duration={1000} direction='right'><h1 className='text-3xl'>Alternative Product FAQs: Your Questions Answered</h1></Fade>
+                <Fade duration={1000} direction='left'><div className='bg-primary-color h-[2px] mt-3 mb-8 '></div></Fade>
                 {/* accordinia */}
-                <div className="join join-vertical w-full">
-                    <div className="collapse collapse-arrow join-item border border-base-300">
-                        <input type="radio" name="my-accordion-4" defaultChecked />
-                        <div className="collapse-title text-xl font-medium">
-                            Why should I consider alternative products?
+                <Fade duration={1000} direction='up'>
+                    <div className="join join-vertical w-full">
+                        <div className="collapse collapse-arrow join-item border border-base-300">
+                            <input type="radio" name="my-accordion-4" defaultChecked />
+                            <div className="collapse-title text-xl font-medium">
+                                Why should I consider alternative products?
+                            </div>
+                            <div className="collapse-content">
+                                <p>Alternative products can offer various benefits such as cost savings, environmental sustainability, unique features, or ethical considerations. They provide consumers with more choices and options to suit their preferences and values.</p>
+                            </div>
                         </div>
-                        <div className="collapse-content">
-                            <p>Alternative products can offer various benefits such as cost savings, environmental sustainability, unique features, or ethical considerations. They provide consumers with more choices and options to suit their preferences and values.</p>
+                        <div className="collapse collapse-arrow join-item border border-base-300">
+                            <input type="radio" name="my-accordion-4" />
+                            <div className="collapse-title text-xl font-medium">
+                                How do I know if an alternative product is right for me?
+                            </div>
+                            <div className="collapse-content">
+                                <p>Consider factors such as your preferences, needs, budget, and values when evaluating alternative products. Look for features or attributes that align with your priorities and conduct thorough research to make an informed decision.</p>
+                            </div>
+                        </div>
+                        <div className="collapse collapse-arrow join-item border border-base-300">
+                            <input type="radio" name="my-accordion-4" />
+                            <div className="collapse-title text-xl font-medium">
+                                Can I trust alternative product recommendations from online sources?
+                            </div>
+                            <div className="collapse-content">
+                                <p>It's essential to critically evaluate alternative product recommendations from online sources, considering factors such as credibility, transparency, and relevance. Look for reviews from trusted sources, consider multiple opinions, and verify information to make well-informed decisions.</p>
+                            </div>
+                        </div>
+                        <div className="collapse collapse-arrow join-item border border-base-300">
+                            <input type="radio" name="my-accordion-4" />
+                            <div className="collapse-title text-xl font-medium">
+                                How can I determine the environmental impact of alternative products?
+                            </div>
+                            <div className="collapse-content">
+                                <p>This question delves into factors such as carbon footprint, energy efficiency, recyclability, and sustainable sourcing practices, helping users make environmentally conscious choices.</p>
+                            </div>
+                        </div>
+                        <div className="collapse collapse-arrow join-item border border-base-300">
+                            <input type="radio" name="my-accordion-4" />
+                            <div className="collapse-title text-xl font-medium">
+                                How do I know if an alternative product is ethically produced?
+                            </div>
+                            <div className="collapse-content">
+                                <p>This question examines ethical considerations such as fair labor practices, animal welfare standards, and supply chain transparency, empowering users to make socially responsible choices.</p>
+                            </div>
                         </div>
                     </div>
-                    <div className="collapse collapse-arrow join-item border border-base-300">
-                        <input type="radio" name="my-accordion-4" />
-                        <div className="collapse-title text-xl font-medium">
-                            How do I know if an alternative product is right for me?
-                        </div>
-                        <div className="collapse-content">
-                            <p>Consider factors such as your preferences, needs, budget, and values when evaluating alternative products. Look for features or attributes that align with your priorities and conduct thorough research to make an informed decision.</p>
-                        </div>
-                    </div>
-                    <div className="collapse collapse-arrow join-item border border-base-300">
-                        <input type="radio" name="my-accordion-4" />
-                        <div className="collapse-title text-xl font-medium">
-                            Can I trust alternative product recommendations from online sources?
-                        </div>
-                        <div className="collapse-content">
-                            <p>It's essential to critically evaluate alternative product recommendations from online sources, considering factors such as credibility, transparency, and relevance. Look for reviews from trusted sources, consider multiple opinions, and verify information to make well-informed decisions.</p>
-                        </div>
-                    </div>
-                    <div className="collapse collapse-arrow join-item border border-base-300">
-                        <input type="radio" name="my-accordion-4" />
-                        <div className="collapse-title text-xl font-medium">
-                            How can I determine the environmental impact of alternative products?
-                        </div>
-                        <div className="collapse-content">
-                            <p>This question delves into factors such as carbon footprint, energy efficiency, recyclability, and sustainable sourcing practices, helping users make environmentally conscious choices.</p>
-                        </div>
-                    </div>
-                    <div className="collapse collapse-arrow join-item border border-base-300">
-                        <input type="radio" name="my-accordion-4" />
-                        <div className="collapse-title text-xl font-medium">
-                            How do I know if an alternative product is ethically produced?
-                        </div>
-                        <div className="collapse-content">
-                            <p>This question examines ethical considerations such as fair labor practices, animal welfare standards, and supply chain transparency, empowering users to make socially responsible choices.</p>
-                        </div>
-                    </div>
-                </div>
+                </Fade>
             </div>
 
             <div className='mt-20 mb-12 space-y-10'>
@@ -205,7 +223,7 @@ const Home = () => {
 
 
             <div>
-               
+
 
                 {/* <FrameMotion></FrameMotion> */}
 
@@ -225,7 +243,7 @@ const Home = () => {
                 </motion.div> */}
 
             </div>
-{/* 
+            {/* 
             <div>
                 <NavBar2></NavBar2>
             </div> */}
